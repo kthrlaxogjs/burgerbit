c095001a1de9d1ca42e7e7ceebf47c1fa813d5e2
kthrlaxogjs <kth20050130@gmail.com>
2024-04-11 01:42:18 +0900

    start

---
+++ burgerbit/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Mainpage></Mainpage>
+      </div>
+    );
+  }
+}
+
+class Mainpage extends Component {
+  render() {
+    return (
+      <div className="Mainpage">
+        burgerasfdsafd
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
